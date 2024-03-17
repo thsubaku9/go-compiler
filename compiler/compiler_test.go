7e4f343f279bee443c101c115a352227182a5682
kaustubh <kaustubhkj@gmail.com>
2024-03-17 13:30:21 +0530

    need to resolve test

--- compiler/compiler_test.go
+++ compiler/compiler_test.go
@@ -2,10 +2,65 @@ package compiler
 
 import (
 	"monkey-c/code"
+	"monkey-i/ast"
+	"monkey-i/lexer"
+	"monkey-i/object"
+	"monkey-i/parser"
+	"testing"
 )
 
 type compilerTestCase struct {
 	input                string
 	expectedConstants    []interface{}
 	expectedInstructions []code.Instructions
+}
+
+func TestIntCalculation(t *testing.T) {
+	tests := []compilerTestCase{
+		{
+			input:             "1 + 2",
+			expectedConstants: []interface{}{1, 2},
+			expectedInstructions: []code.Instructions{
+				code.Make(code.OpConstant, 0),
+				code.Make(code.OpConstant, 1),
+			},
+		},
+	}
+
+	runCompilerTests(t, tests)
+}
+
+func runCompilerTests(t *testing.T, tests []compilerTestCase) {
+	t.Helper()
+	for _, tt := range tests {
+		program := parse(tt.input)
+		compiler := New()
+		err := compiler.Compile(program)
+		if err != nil {
+			t.Fatalf("compiler error: %s", err)
+		}
+		bytecode := compiler.Bytecode()
+		err = testInstructions(tt.expectedInstructions, bytecode.Instructions)
+		if err != nil {
+			t.Fatalf("testInstructions failed: %s", err)
+		}
+		err = testConstants(t, tt.expectedConstants, bytecode.Constants)
+		if err != nil {
+			t.Fatalf("testConstants failed: %s", err)
+		}
+	}
+}
+
+func parse(input string) *ast.Program {
+	l := lexer.New(input)
+	p := parser.New(l)
+	return p.ParseProgram()
+}
+
+func testInstructions(expected []code.Instructions, actual code.Instructions) error {
+	return nil
+}
+
+func testConstants(t *testing.T, expected []interface{}, actual []object.Object) error {
+	return nil
 }
