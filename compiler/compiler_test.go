dc7a7b9ef6f06f073a55211ae1f42cac4b75206a
kaustubh <kaustubhkj@gmail.com>
2024-03-17 17:45:02 +0530

    need to write code for same 🫠

--- compiler/compiler_test.go
+++ compiler/compiler_test.go
@@ -1,6 +1,7 @@
 package compiler
 
 import (
+	"fmt"
 	"monkey-c/code"
 	"monkey-i/ast"
 	"monkey-i/lexer"
@@ -57,10 +58,55 @@ func parse(input string) *ast.Program {
 	return p.ParseProgram()
 }
 
+func concatInstructions(s []code.Instructions) code.Instructions {
+	out := code.Instructions{}
+	for _, ins := range s {
+		out = append(out, ins...)
+	}
+	return out
+}
+
 func testInstructions(expected []code.Instructions, actual code.Instructions) error {
+
+	concatted := concatInstructions(expected)
+	if len(actual) != len(concatted) {
+		return fmt.Errorf("wrong instructions length.\nwant=%q\ngot =%q",
+			concatted, actual)
+	}
+	for i := range concatted {
+		if actual[i] != concatted[i] {
+			return fmt.Errorf("wrong instruction at %d.\nwant=%q\ngot =%q", i, concatted, actual)
+		}
+	}
+
 	return nil
 }
 
 func testConstants(t *testing.T, expected []interface{}, actual []object.Object) error {
+	if len(expected) != len(actual) {
+		return fmt.Errorf("wrong number of constants. got=%d, want=%d",
+			len(actual), len(expected))
+	}
+
+	for i, constant := range expected {
+		switch constant := constant.(type) {
+		case int:
+			err := testIntegerObject(int64(constant), actual[i])
+			if err != nil {
+				return fmt.Errorf("constant %d - testIntegerObject failed: %s", i, err)
+			}
+		}
+	}
+	return nil
+}
+
+func testIntegerObject(expected int64, actual object.Object) error {
+	result, ok := actual.(*object.Integer)
+	if !ok {
+		return fmt.Errorf("object is not Integer. got=%T (%+v)", actual, actual)
+	}
+	if result.Value != expected {
+		return fmt.Errorf("object has wrong value. got=%d, want=%d", result.Value, expected)
+	}
 	return nil
 }
