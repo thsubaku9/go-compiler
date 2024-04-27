c3247e6f544fa54bfc7ca2b5314a92673e30b339
kaustubh <kaustubhkj@gmail.com>
2024-04-27 19:18:33 +0530

    resolving scopes

--- compiler/symbol_table.go
+++ compiler/symbol_table.go
@@ -4,6 +4,7 @@ type SymbolScope string
 
 const (
 	GlobalScope SymbolScope = "G"
+	LocalScope  SymbolScope = "L"
 )
 
 type Symbol struct {
@@ -13,6 +14,7 @@ type Symbol struct {
 }
 
 type SymbolTable struct {
+	Outer   *SymbolTable
 	store   map[string]Symbol
 	numDefs int
 }
@@ -21,9 +23,17 @@ func NewSymbolTable() *SymbolTable {
 	return &SymbolTable{store: make(map[string]Symbol)}
 }
 
+func NewEnclosedSymbolTable(outer *SymbolTable) *SymbolTable {
+	return &SymbolTable{store: make(map[string]Symbol), Outer: outer}
+}
+
 func (symt *SymbolTable) Define(val string) Symbol {
 
 	symbol := Symbol{Name: val, Index: symt.numDefs, Scope: GlobalScope}
+
+	if symt.Outer != nil {
+		symbol.Scope = LocalScope
+	}
 	symt.store[val] = symbol
 	symt.numDefs++
 	return symbol
@@ -31,5 +41,8 @@ func (symt *SymbolTable) Define(val string) Symbol {
 
 func (symt *SymbolTable) Resolve(val string) (Symbol, bool) {
 	obj, ok := symt.store[val]
+	if !ok && symt.Outer != nil {
+		obj, ok = symt.Outer.Resolve(val)
+	}
 	return obj, ok
 }
